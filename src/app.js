95088aaf64a9a522f707fb5488c1d0ec838f4367
Sankar Narayanan <953622104089@ritrjpm.ac.in>
2026-06-03 15:18:45 +0530

    Fix CORS

--- src/app.js
+++ src/app.js
@@ -2,20 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  process.env.CLIENT_URL,
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
 
 app.use(express.json());
